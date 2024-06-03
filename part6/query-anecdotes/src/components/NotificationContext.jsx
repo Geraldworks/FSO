77e564e88b5ddd572fde981037de3951e5603ce7
Gerald Ho <97504394+Geraldworks@users.noreply.github.com>
2024-06-03 23:40:39 +0800

    completed 7.1 - 7.3

--- part6/query-anecdotes/src/components/NotificationContext.jsx
+++ part6/query-anecdotes/src/components/NotificationContext.jsx
@@ -20,7 +20,7 @@ export const NotificationContextProvider = (props) => {
   );
 
   // useCallback hook to memoize callbacks so dont have to call them again
-  // can also use to create custom functionality which otherwise 
+  // can also use to create custom functionality which otherwise
   // would not be possible as hooks need to be used in components or in custom components
   const setNotifAndTimeout = useCallback((message) => {
     notificationDispatch({ type: "SET", payload: message });
@@ -28,6 +28,9 @@ export const NotificationContextProvider = (props) => {
   }, []);
 
   return (
+    // important to provide the context as having context is not enough, you
+    // need to still provides the values associated with the context
+    // which is only available within the provider
     <NotificationContext.Provider value={[notification, setNotifAndTimeout]}>
       {props.children}
     </NotificationContext.Provider>
